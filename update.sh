798d20ea34cf64578b964ebd13312a87c3fde349
Owen <qcgzxw@qq.com>
2018-05-01 15:57:06 +0800

    5.1
    5.1

--- update.sh
+++ update.sh
@@ -28,7 +28,7 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/PUBG-Radar-Onekey.git
+git clone https://github.com/qcgzxw/scichicken.git
 cd PUBG-Radar-Onekey/
 npm i
 npm i -g pino
